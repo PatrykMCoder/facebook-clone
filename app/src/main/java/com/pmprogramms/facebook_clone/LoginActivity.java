dec70e7ed2e137c1a4799757362aa58d75e8dff4
PatrykMCoder <patrykmarciszewski3@gmail.com>
2021-01-01 17:34:31 +0100

    Create layout for register and login activity

--- app/src/main/java/com/pmprogramms/facebook_clone/LoginActivity.java
+++ app/src/main/java/com/pmprogramms/facebook_clone/LoginActivity.java
@@ -2,15 +2,13 @@ package com.pmprogramms.facebook_clone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-    
+public class LoginActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        startActivity(new Intent(this, RegisterActivity.class));
+        setContentView(R.layout.activity_login);
     }
 }
